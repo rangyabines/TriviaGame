fc328972cb386b8b15954688d4a86652aa0bf9d7
Rangy Abines <rangyabines11@gmail.com>
2016-06-04 21:56:03 -0400

    updated files

---
+++ assets/javascript/json.js
@@ -0,0 +1,10 @@
+// JSON Document
+data = {"quizcontent": [
+        {"question": "How many siblings did Eddard Stark have?", "image": "1.jpg", "correct": 1, "a1":"Three", "a2":"Two","a3":"Five","a4":"Four"},
+    {"question": "What is the name of the eldest child of Mad King Aerys?", "image": "2.jpg", "correct": 3, "a1":"Rhaegys", "a2":"Rhaego","a3":"Rhaegar","a4":"Rhaegerys"},
+	{"question": "What is the name of Robert's youngest brother?", "image": "3.jpg", "correct": 4, "a1":"Reigh", "a2":"Reihly","a3":"Reley","a4":"Renly"},
+	{"question": "What is the Lannister family's sigil?", "image": "4.jpg", "correct": 4, "a1":"Bear", "a2":"Dragon","a3":"Tiger","a4":"Lion"}
+    ],
+	
+};
+
